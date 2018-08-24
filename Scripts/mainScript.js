5426a43cc8a65b15917638e17202872a6066c63d
Kelly Irish <kellygibeaut@gmail.com>
2018-08-24 09:20:21 -0700

    Added broken to the query stirng.
    Added the ability to send in a variable broken=true/false so we will not need to upload the broken pages before the event, we can use the boolean.
    
    Defaults to broken=false

--- Scripts/mainScript.js
+++ Scripts/mainScript.js
@@ -9,10 +9,22 @@ var hints = [
   "Hint 7",
   "Hint 8"];
 var totalHints = 8;
-var broken = false;
 var lastClick = new Date($.now());
 var totalClicks = 0;
 
+function getParameters() {
+  var urlParams = new URLSearchParams(window.location.search);
+
+  var isBroken = urlParams.get('broken');
+
+  var sessionBroken = sessionStorage.getItem("isBroken");
+
+   if (isBroken != null) {
+     sessionStorage.setItem("isBroken", isBroken);
+   } else if (sessionBroken == null) {
+     sessionStorage.setItem("isBroken", "false");
+   }
+}
 
 function addScroll() {
   $(window).scroll(function(e){
@@ -32,7 +44,8 @@ function addListeners() {
   var $mission = $('#mission_fix');
   var $ticket = $('#ticket_fix');
   var $pop_up = $('.pop_up');
-  if (broken)
+  var broken = sessionStorage.getItem("isBroken");
+  if (broken == "true")
   {
     $mission.text("Our Misssssion");
     $mission.on('click', function() { fixMission() });
@@ -42,12 +55,19 @@ function addListeners() {
 }
 
 function fixMission() {
+var broken = sessionStorage.getItem("isBroken");
+if (broken == "true")
+{
   var $mission = $('#mission_fix');
   $mission.text("Our Mission");
     $('.asset_2').show();
+  }
 }
 
 function ticketClick() {
+var broken = sessionStorage.getItem("isBroken");
+if (broken == "true")
+{
   var currentClick = new Date($.now());
   var clickDifference = currentClick - lastClick;
   lastClick = currentClick;
@@ -60,16 +80,24 @@ function ticketClick() {
     return false;
   }
 }
+}
 
 function StartGetGeeked(){
+
+  var broken = sessionStorage.getItem("isBroken");
+  if (broken == "true")
+  {
   var start = sessionStorage.getItem('start_get_geeked');
   if (start != "false") {
     console.log("Welcome to Get Geeked in Tech QA.");
     console.log("Type GetHint() to get a hint.");
-}
+  }
   sessionStorage.setItem("start_get_geeked", "false");
-
+} else {
+  console.log("To start the test activity go to /index.html?broken=true");
 }
+}
+
 
 function ResetHints() {
   sessionStorage.setItem("hints", JSON.stringify(hints));
